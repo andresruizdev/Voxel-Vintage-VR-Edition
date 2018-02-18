c2e373f14dbf01f09adef5d0f2ff4d1f5c40c5c3
Andres Ruiz <rb.andres98@gmail.com>
2018-02-17 21:37:55 -0500

    Add local project to GitKraken

---
+++ Assets/Voxel Vintage/Scripts/Objects Movement/ObjectsMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsMovement : MonoBehaviour
+{
+    private float speed;
+
+    private void Start()
+    {
+        speed = 1f;
+        StartCoroutine(GravityMovement());
+        StartCoroutine(TurningCoroutine());
+    }
+    #region TurningCoroutine
+    private IEnumerator TurningCoroutine()
+    {
+        for (int i = 0; i < 360; i++)
+        {
+            transform.Rotate(Vector3.up * speed, Space.World);
+            yield return new WaitForSeconds(0.0001f);
+        }
+        StartCoroutine(TurningCoroutine());
+    }
+    #endregion
+
+    #region
+    private IEnumerator GravityMovement()
+    {
+        for (int i = 0; i < 200; i++)
+        {
+            if (i < 100)
+            {
+                transform.position += new Vector3(0, 0.01f, 0);
+            }
+            else if (i > 99)
+            {
+                transform.position -= new Vector3(0, 0.01f, 0);
+            }
+            yield return new WaitForSeconds(.00001f);
+        }
+        StartCoroutine(GravityMovement());
+    }
+    #endregion
+}
